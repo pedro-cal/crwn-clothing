ccdb032d8138e369f52fb16016f1953dcab1a058
pedro-cal <pedroluna2@hotmail.com>
2020-09-10 10:51:10 -0300

    added sign-up with email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,31 @@ const config = {
     measurementId: "G-K2D52W1W1V"
   };
 
+//* NEEDS FURTHER EXPLORATION 
+//* Will be called in App.js in componentDidMount unsubscribeFromAuth 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if(!snapshot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
